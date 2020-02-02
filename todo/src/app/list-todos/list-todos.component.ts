be0b1bbe12d9e966bf2e0be1094eae078a9cfad7
jindalhi <jindal.heenu@gmail.com>
2020-02-02 14:37:59 -0500

    Added bootstrap in Styles.css

--- todo/src/app/list-todos/list-todos.component.ts
+++ todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo{
+  constructor(
+    public id:number,
+    public description:string,
+    public done: boolean,
+    public targetDate: Date
+  ){
+
+  }
+}
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,15 +18,11 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id: 1, description: 'Learn to Dance'},
-    {id: 2, description: 'Learn to Swim'},
-    {id: 3, description: 'Become an expert at Angular'}
+    new Todo(1, 'Learn to Dance', false, new Date()),
+    new Todo(2, 'Become an expert at Angular', false, new Date()),
+    new Todo(3, 'Learn to Swim', false, new Date())
   ]
-  todo = {
-    id: 1,
-    description: 'Learn to Dance'
-  }
-  
+
   constructor() { }
 
   ngOnInit() {
