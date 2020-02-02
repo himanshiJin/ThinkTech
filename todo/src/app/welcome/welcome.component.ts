3ac491654f14609a0488383d1dad5eefb0d01551
jindalhi <jindal.heenu@gmail.com>
2020-02-01 21:33:25 -0500

    Added Welcome and Login Components

---
+++ todo/src/app/welcome/welcome.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  message = 'Some Welcome Message'
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.message)
+  }
+
+}
+
+export class Class1{
+
+}
+
+export class Class2{
+
+}
