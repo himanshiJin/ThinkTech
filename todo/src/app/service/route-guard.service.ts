25a2577a56651335c888ffbca6269e45d9753374
jindalhi <jindal.heenu@gmail.com>
2020-02-02 23:09:44 -0500

    Prevented access to components if not logged in using routing service

--- todo/src/app/service/route-guard.service.ts
+++ todo/src/app/service/route-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
 
 @Injectable({
@@ -7,12 +7,15 @@ import { HardcodedAuthenticationService } from './hardcoded-authentication.servi
 })
 export class RouteGuardService implements CanActivate{
 
-  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService,
+    private router: Router) { }
   
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-      if (this.hardcodedAuthenticationService.isUserLoggedIn()){
+      if (this.hardcodedAuthenticationService.isUserLoggedIn())
         return true;
-      }
+        
+      this.router.navigate(['login']);
+      
       return false;
   }
 }
