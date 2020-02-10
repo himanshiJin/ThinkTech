b904e5c53e3be8a79adff5d2de24a02c058131f8
jindalhi <jindal.heenu@gmail.com>
2020-02-09 20:25:26 -0500

    Setup restful services project with springboot

---
+++ restful-web-services/src/test/java/com/himanshi/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshi.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
