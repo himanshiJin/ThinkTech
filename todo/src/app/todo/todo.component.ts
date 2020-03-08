08c72b5a44678301f9c7f525ed905850ce829fc7
jindalhi <jindal.heenu@gmail.com>
2020-03-08 16:23:19 -0400

    Implemented Add Todo Feature

--- todo/src/app/todo/todo.component.ts
+++ todo/src/app/todo/todo.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TodoDataService } from '../service/data/todo-data.service';
 import { Todo } from '../list-todos/list-todos.component';
+import { THROW_IF_NOT_FOUND } from '@angular/core/src/di/injector';
 
 @Component({
   selector: 'app-todo',
@@ -15,14 +16,39 @@ export class TodoComponent implements OnInit {
 
   constructor(
     private route:ActivatedRoute,
-    private todoservice: TodoDataService
+    private todoservice: TodoDataService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
+    
+    this.todo = new Todo(this.id,'',false, new Date())
+
+    if(this.id!=-1){
     this.todoservice.retrieveTodo('himanshi', this.id).subscribe(
       data => this.todo = data
     )
   }
+}
+
+  saveTodo(){
+    if(this.id == -1){
+      this.todoservice.createTodo('himanshi',this.todo)
+      .subscribe(
+        data=>{
+          console.log(data)
+          this.router.navigate(['todos'])
+        }
+      )
+    }else{
+    this.todoservice.updateTodo('himanshi',this.id,this.todo).subscribe(
+      data => {
+        console.log(data)
+        this.router.navigate(['todos'])
+      }
+    )
+  }
+}
 
 }
