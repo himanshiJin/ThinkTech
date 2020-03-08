0db1ccd311f3e00316d410b097d1b5c6581481fd
jindalhi <jindal.heenu@gmail.com>
2020-03-08 19:49:00 -0400

    Configured Spring Security

--- todo/src/app/service/data/welcome-data.service.ts
+++ todo/src/app/service/data/welcome-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 export class HelloWorldBean{
@@ -21,7 +21,21 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldBeanServiceWithPathVariable(name){
-    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`);
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+    
+    let headers = new HttpHeaders({
+        Authorization: basicAuthHeaderString
+    })
+    return this.http.get<HelloWorldBean>(`http://localhost:8080/hello-world/path-variable/${name}`, 
+    {headers});
     //console.log("Execute Hello World Bean Service");
   }
+
+createBasicAuthenticationHttpHeader(){
+  let username = 'himanshi'
+  let password = 'dummy'
+  let basicAuthHeaderString = 'Basic '+ window.btoa(username + ':' + password);
+  return basicAuthHeaderString;
+}
+
 }
